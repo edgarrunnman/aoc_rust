1c9457bb0047e0ba5aaa3451167d2c555222f8ae
Edgar Runnman <edgar.runnman@lm.se>
2023-10-16 10:42:45 +0200

    progress

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,7 @@ async fn main() {
     dotenv().ok();
     let input_service = InputDataService::new();
     let input = input_service.get_input(String::from("2022"), String::from("1"));
-    // let mut file = File::create("inputs/foo.txt").unwrap();
-    //
-    // file.write(input.await.as_bytes());
-    //
-    // let foo = Local::now();
-    println!("{}", input.await);
+    println!("{}", input.await.unwrap());
 }
 
 struct InputDataService {
@@ -37,17 +32,20 @@ impl InputDataService {
             session_token,
         }
     }
-    async fn get_input(&self, year: String, day: String) -> String {
+    async fn get_input(&self, year: String, day: String) -> Result<String, String> {
         let h = calculate_hash(&self.session_token);
-        let file_name = format!("{}_{}_{}.txt", h, year, day);
+        let file_name = format!("inputs/{}_{}_{}.txt", h, year, day);
         let mut _input = String::new();
-        let file = OpenOptions::new().read(true).open(&file_name).as_mut();
+        let mut file = OpenOptions::new().read(true).open(&file_name);
+        let file = file.as_mut();
 
-        match file {
+        let input_status = match file {
             Ok(x) => {
-                x.read_to_string(&mut _input);
+                println!("reading from file");
+                x.read_to_string(&mut _input)
             }
             Err(_) => {
+                println!("reading from api");
                 let url = format!("{}/{}/day/{}/input", &self.root_url, year, day);
                 let session_cookie = format!("session={}", &self.session_token);
                 let res = &self
@@ -62,23 +60,13 @@ impl InputDataService {
                     .unwrap();
                 let mut file = File::create(file_name).unwrap();
                 _input = res.to_string();
-                file.write(res.to_string().as_bytes());
+                file.write(res.to_string().as_bytes())
             }
         };
-        // let url = format!("{}/{}/day/{}/input", &self.root_url, year, day);
-        // let session_cookie = format!("session={}", &self.session_token);
-        // let res = &self
-        //     .client
-        //     .get(url)
-        //     .header("cookie", session_cookie)
-        //     .send()
-        //     .await
-        //     .unwrap()
-        //     .text()
-        //     .await
-        //     .unwrap();
-        // res.to_string()
-        _input
+        match input_status {
+            Ok(_) => Ok(_input),
+            Err(_) => Err(String::from("Error")),
+        }
     }
 }
 
