40c53038011b2c2f8109752fc13006ad89891a86
Edgar Runnman <edgar.runnman@lm.se>
2023-11-16 17:32:13 +0100

    progress

--- src/solutions/solution_2022_5.rs
+++ src/solutions/solution_2022_5.rs
@@ -6,10 +6,29 @@ pub struct SolutionImp {
 
 impl Solution for SolutionImp {
     fn solution_part_1(&self) -> Option<String> {
+        let (data, commands) = to_data_and_commands(&self.input);
+        make_stacks(&data);
         None
     }
 
     fn solution_part_2(&self) -> Option<String> {
         None
     }
+}
+
+fn to_data_and_commands(input: &String) -> (String, String) {
+    let data = input.split("\n\n").collect::<Vec<&str>>();
+    (String::from(data[0]), String::from(data[1]))
+}
+
+fn make_stacks(input: &String) -> Vec<String> {
+    let lines = input.lines().rev().skip(1);
+    for line in &lines.clone().collect::<Vec<&str>>() {
+        println!("{:?}", line);
+    }
+    let stacks = (0..(lines.collect::<Vec<&str>>()[0].len() + 1) / 4)
+        .map(|n| n.to_string())
+        .collect::<Vec<String>>();
+    println!("{:?}", stacks);
+    vec![]
 }
