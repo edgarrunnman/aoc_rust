86007c94143504cde572d18a23df495836655a39
Edgar Runnman <edgar.runnman@lm.se>
2023-11-17 14:32:21 +0100

    progress

--- src/solutions/solution_2022_6.rs
+++ src/solutions/solution_2022_6.rs
@@ -6,6 +6,21 @@ pub struct SolutionImp {
 
 impl Solution for SolutionImp {
     fn solution_part_1(&self) -> Option<String> {
+        for index in 4..self.input.len() {
+            let slice = ['r', 'u', 's', 't'];
+            let mut iter = slice.windows(2);
+            let mut foo = self.input[(index - 4)..index]
+                .chars()
+                .into_iter()
+                .collect::<Vec<char>>()
+                .clone();
+            print!("sliece [{} - {}] : {:?}", index - 4, index, foo);
+            let foo = foo.drain(..);
+            // if foo.len() == 4 {
+            //     return Some(index.to_string());
+            // };
+            println!(" => {:?}", foo);
+        }
         // let foo = self.input[0..4];
         // println!("{:?}", foo);
         None
